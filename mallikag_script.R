919c77234a5b6fb8b422bd2abff8657e59a85013
mgupta106 <mgupta22@gmu.edu>
2021-11-23 22:19:47 -0500

    finished visualizations

--- mallikag_script.R
+++ mallikag_script.R
@@ -15,6 +15,8 @@ DRC <- as.data.frame(DRC)
 
 agrland<- lapply(DRC$agrland, as.numeric)
 
+
+
 ## Plotting Urban Population by Year Using Plot Function
 
 DRC$Year <- as.numeric(as.character(DRC$Year))
@@ -76,7 +78,24 @@ h
 ## agriculture and grow different types of crops. In the years 2010-2018, the agricultural land 
 ## increased to a value of 315000 km.
 
+## Line Chart
 
+ggplot(DRC, aes(x=Year, y=CO2emissionsgaseouskt)) +
+  geom_line(color="purple")
+
+## Explanation of Line Chart
+
+## From the years of 1960-2018, the CO2 emissions are shown to be constant around the value zero.
+## Around the year 2016, the CO2 emissions value is seen to be 3.667 per kiloton. When the year was 
+## 2010, the CO2 gases emitted are seen to be 44.004. There are N/A values between the years of 
+## 2017-2020. 
+
+## How it Relates to Other Variables
+
+## In the years 1960-1990, the CO2 emissions are seen to be constant around zero. One of the main 
+## reasons why this could have occurred is because there were not many people which led to the 
+## population to be less. Also, the number of cars driven by the population were less as compared to 
+## the 2000s.
 
 
  
